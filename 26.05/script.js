055d8f12984173ecedd3943d283123570388aa7e
Ismatsan <ismat7300@gmail.com>
2022-05-26 19:42:18 +0600

    feet: added script file and writed program on script

---
+++ 26.05/script.js
@@ -0,0 +1,15 @@
+const whois = prompt('Кто пришел?', 'Админ');
+if (whois ==='Админ'){
+    const password = prompt('Пароль?');
+    if (password ==='Черный Властелин'){
+        alert('Добро пожаловать');
+    } else if (password === null){
+        alert('Вход отменен');
+    } else {
+        alert('Пароль неверен');
+    }
+} else if (whois === null){
+    alert('Вход отменен');
+} else {
+    alert('Я вас не знаю');
+}
