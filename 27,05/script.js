ed49905477875c557caad18a54eeebee61a2f28b
Ismatsan <ismat7300@gmail.com>
2022-05-27 00:53:47 +0600

    feat: solved task victorina

---
+++ 27,05/script.js
@@ -0,0 +1,11 @@
+let a=0;
+let b=0
+let question= prompt('Самый высокий водопад в мире?');
+if (question ==='Анхель') {
+    a= (a + 1);
+} else {}
+let question1= prompt('Самая длинная река в мире?');
+if (question1==='Нил') {
+    a= a +1;
+}
+alert(`Вы ответили правильно на ${a} вопросов`);
