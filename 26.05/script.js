46707e2479590374af0910bb01c7f932da0e69d8
Ismatsan <ismat7300@gmail.com>
2022-05-26 23:41:57 +0600

    feat: solved task

--- 26.05/script.js
+++ 26.05/script.js
@@ -1,3 +1,4 @@
+/*
 const whois = prompt('Кто пришел?', 'Админ');
 if (whois ==='Админ'){
     const password = prompt('Пароль?');
@@ -12,4 +13,15 @@ if (whois ==='Админ'){
     alert('Вход отменен');
 } else {
     alert('Я вас не знаю');
+}*/
+
+const test1 = true;
+if (test1 != true){
+    alert('Верно');
+} else {
+    alert('Неверно');
 }
+
+const test2 = true;
+const result = test2 !==true ? 'Верно' : 'Неверно';
+alert(result);
