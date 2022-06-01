9481fa4da5dc12105cf0b562b37d4dac3c72588c
Ismatsan <ismat7300@gmail.com>
2022-06-01 23:56:24 +0600

    feat: solved 3 task

--- 31.05/script.js
+++ 31.05/script.js
@@ -1,11 +1,27 @@
 /** first task **/
-let userSalaries ={
+const userSalaries ={
     Ella: 5000,
     Sophia: 7000,
     Ellie: 8000,
 }
 delete userSalaries.Ella;
-alert(userSalaries);
 console.log(userSalaries);
 
 /** second task **/
+
+let object ={
+    a: 5,
+    b: 6,
+    c: 10,
+}
+let sum =0;
+for (let key in object) {
+    sum += object[key];
+}
+alert(`Всего ${sum}`);
+console.log(`Всего ${sum}`);
+
+/** third task **/
+
+let number = (31 === true) ? number =3 : number = 5;
+
