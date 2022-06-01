b504767caededfa7a1564ff6954c3db1a3143520
Ismatsan <ismat7300@gmail.com>
2022-06-01 21:54:22 +0600

    init: start home work

---
+++ 31.05/script.js
@@ -0,0 +1,11 @@
+/** first task **/
+let userSalaries ={
+    Ella: 5000,
+    Sophia: 7000,
+    Ellie: 8000,
+}
+delete userSalaries.Ella;
+alert(userSalaries);
+console.log(userSalaries);
+
+/** second task **/
