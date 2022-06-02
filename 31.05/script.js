a751c27bdef5f8090e60897ed7293580f00ef9fa
Ismatsan <ismat7300@gmail.com>
2022-06-02 18:49:54 +0600

    feat: added 4th task

--- 31.05/script.js
+++ 31.05/script.js
@@ -8,7 +8,6 @@ delete userSalaries.Ella;
 console.log(userSalaries);
 
 /** second task **/
-
 let object ={
     a: 5,
     b: 6,
@@ -22,6 +21,29 @@ alert(`Всего ${sum}`);
 console.log(`Всего ${sum}`);
 
 /** third task **/
-
 let number = (31 === true) ? number =3 : number = 5;
 
+
+/** forth task **/
+
+const userSalaries = {
+    Asankozhoev: 100000,
+    Babanov: 15000,
+    Matraimov: 1000000,
+    Atambaev: 150000,
+    Keldibekov: 545212,
+    Akaev: 100000,
+    Jeenbekov: 90000,
+    Sariev: 70000,
+    Motuev: 40000,
+    Japarov: 200000,
+
+
+}
+let sum =0;
+for (let key in userSalaries) {
+    sum += userSalaries[key];
+}
+
+alert(`Всего ${sum} сомов`);
+console.log(`Всего ${sum} сомов`);
