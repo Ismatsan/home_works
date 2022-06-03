adffae5e8005def429f717207a68d2bfb5fde89d
Ismatsan <ismat7300@gmail.com>
2022-06-03 23:57:26 +0600

    feat: solved_task_#2

--- 03,06/script.js
+++ 03,06/script.js
@@ -4,3 +4,16 @@ let arr = ['a', 'b', 'c', 'd'];
 console.log(arr[0] +arr[1],',', arr[2] +arr[3]);
 
 /** second task **/
+// let mass = [10, 15, 9, 10];
+// let a =[mass[0] *mass[1]];
+// let b =[mass[2] *mass[3]];
+// let result =a +b;
+// console.log(result);
+
+let mas = [10, 15, 9, 10];
+let result1 = [(mas[0] *mas[1]) + (mas[2] *mas[3])]
+
+alert(result1);
+console.log(result1);
+
+/** task 3 **/
