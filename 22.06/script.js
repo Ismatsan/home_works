1c5f3080136d54b0757dc9ca2fad3d5f6f00a2f6
Ismatsan <ismat7300@gmail.com>
2022-06-22 23:21:06 +0600

    feat: completed discription.

---
+++ 22.06/script.js
@@ -0,0 +1,32 @@
+/** задача на метод reduce **/
+
+/*const numbers = [1,2,23,5,45];
+
+const result = numbers.reduce((sum, item)=>{
+    return sum + item;
+}, 0)
+console.log(result);*/
+
+/** Описание **/
+/*
+1) (( sum = 1; item = 2) =>{
+    return 1 + 2;
+}, 3)
+
+2) ((sum = 3; item =23) =>{
+    return 3 + 23;
+}, 26)
+
+3) ((sum = 26; item =5) =>{
+    return 26 + 5;
+}, 31)
+
+4)((sum = 31; item = 45) =>{
+    return 31 + 45;
+}, 76)
+console.log(result) //76
+*/
+
+/** 1st task
+ * Напишите функцию mul(n,m), которая принимает два аргумента и возвращает
+ произведение этих аргументов. **/
