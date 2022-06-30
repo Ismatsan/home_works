d8c2a6c395a80dc0d4fdb2c2161f97d0b05ca215
Ismatsan <ismat7300@gmail.com>
2022-06-30 18:26:54 +0600

    init: start home_work

---
+++ 29,06/script.js
@@ -0,0 +1,6 @@
+
+
+const product_name = document.getElementById('product-name');
+const product_price = document.getElementsByClassName(product_price)[0];
+const product_description = document.getElementsByTagName('p')[0];
+console.log(product_description);
