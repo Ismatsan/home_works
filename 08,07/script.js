4d2429d0d1ff5fd3ff929cc76b69ada61624eb61
Ismatsan <ismat7300@gmail.com>
2022-07-11 01:10:04 +0600

    init: start homeWork

--- 08,07/script.js
+++
@@ -1,8 +0,0 @@
-let dives = document.createElement('div');
-dives.className = "alert";
-document.body.prepend(dives);
-dives.innerHTML = 'Мы создали див.Вставляем prepend';
-dives.before('Вставляем before');
-dives.after('Вставляем after');
-
-
