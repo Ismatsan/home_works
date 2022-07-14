76c5ba027d842f0cbdb07cca2e76885d445b5f22
Ismatsan <ismat7300@gmail.com>
2022-07-14 14:30:22 +0600

    feat: homeWork finished

--- Июль/13.07/script.js
+++ Июль/13.07/script.js
@@ -1,10 +1,10 @@
 const phones = [{
-    number: "1",
+    number: "ID: 1",
     name: "Iphone 13 pro max",
     image_url: "https://istore.kg/media/products/iphone-13-pro-max-green-select.webp",
     price: "100250 сом"
 }, {
-    number: "2",
+    number: "ID: 2",
     name: "Iphone 13 pro",
     image_url: "https://istore.kg/media/products/iphone-13-pro-blue-select.webp",
     price: "97000 сом"
@@ -13,27 +13,27 @@ const phones = [{
 
 const container = document.getElementById('store');
 
-phones.forEach((result, idx) => {
+phones.forEach((result, i) => {
     const card = document.createElement('div');
     card.classList = 'card-body';
 
 
 const content = `
   <div class="card">
-    <div class="card-header" id="heading-${idx}">
+    <div class="card-header" id="heading-${i}">
       
-        <button class="btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
-            Купить
+        <button class="btn_link" onclick= "location= '${result.image_url}'">
+            Посмотреть
         </button>
      
     </div>
 
-    <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#store">
+    <div id="collapse-${i}" class="collapse show" aria-labelledby="heading-${i}" data-parent="#store">
       <div class="card-body">
 
         <h5>${result.number}</h5>
         <p>${result.name}</p>
-        <p>${result.image_url}</p>
+        
         <p>${result.price}</p>
         
       </div>
