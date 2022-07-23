c03310dbf3fe4d3edb0a607250501acd3452e66f
Ismatsan <ismat7300@gmail.com>
2022-07-23 18:27:06 +0600

    feat: added required

--- Июль/Megastore/script.js
+++ Июль/Megastore/script.js
@@ -1,97 +1,31 @@
-
-
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', () =>{
-    const userSurnameElement = document.querySelector('#userSurname');
-    const userNameElement = document.querySelector('#userName');
-    const userPatronymicElement = document.querySelector('#userPatronymic');
-    const userBirthdayElement = document.querySelector('#userBirthday');
-    const userResidenceElement = document.querySelector('#userResidence');
-    const serialNumberElement = document.querySelector('#serialNumber')
-    const personalNumberElement = document.querySelector('#personalNumber')
-    const receivingDateElement = document.querySelector('#receivingDate')
-    const contactNumberElement = document.querySelector('#contactNumber')
-    const eMailElement = document.querySelector('#eMail')
-    const userBankElement = document.querySelector('#userBank')
-    const actualResidenceElement = document.querySelector('#actualResidence')
+    const elements = document.querySelectorAll('#form input, #form select, #form textarea');
+    for(let i = 0; i < elements.length; i++){
+        elements[i].value;
+        if(elements[i].hasAttribute('required')){
+            if (elements[i].value === '') {
+                console.log('Поле пустое');
+            }
+        }
 
-    localStorage.userSurname = userSurnameElement.value;
-    localStorage.userName = userNameElement.value;
-    localStorage.userPatronymic = userPatronymicElement.value;
-    localStorage.userBirthday = userBirthdayElement.value;
-    localStorage.userResidence = userResidenceElement.value;
-    localStorage.serialNumber = serialNumberElement.value;
-    localStorage.personalNumber = personalNumberElement.value;
-    localStorage.receivingDate = receivingDateElement.value;
-    localStorage.contactNumber = contactNumberElement.value;
-    localStorage.eMail = eMailElement.value;
-    localStorage.userBank = userBankElement.value;
-    localStorage.actualResidence = actualResidenceElement.value;
+// if(elements.value === '') {
+//     elements.style.boxShadow = "0 0 10px red";
+// }//подсвечивает красным не заполненые поля
 
-    function check(){
-    let surname = document.getElementById('userSurname').value;
-    let name = document.getElementById('userName').value;
-    let patronymic = document.getElementById('userPatronymic').value;
-    let birth = document.getElementById('userBirthday').value;
-    let residence = document.getElementById('userPatronymic').value;
-    let serialNumber = document.getElementById('serialNumber').value;
-    let personalNumber = document.getElementById('personalNumber').value;
-    let receivingDate = document.getElementById('receivingDate').value;
-    let contactNumber = document.getElementById('contactNumber').value;
-    let eMail = document.getElementById('eMail').value;
-    let bank = document.getElementById('userBank').value;
-    let actual = document.getElementById('actualResidence').value;
-    let button = document.getElementById('next');
-    button.disabled = true;
-    if (surname.length != 0 && name.length != 0 && patronymic.length != 0 && birth.length != 0 && residence.length != 0
-    && serialNumber.length != 0 && personalNumber.length != 0 && receivingDate.length != 0 && contactNumber.length !=0
-    && eMail.length != 0 && bank.length != 0 && actual.length != 0) {
-        button.disabled = false;
-    }
-}
-check();
-
-function send() {
-    let sur = document.getElementById('userSurname').value;
-    let name = document.getElementById('userName').value;
-    let patronymic = document.getElementById('userPatronymic').value;
-    let birth = document.getElementById('userBirthday').value;
-    let residence = document.getElementById('userPatronymic').value;
-    let serialNumber = document.getElementById('serialNumber').value;
-    let personalNumber = document.getElementById('personalNumber').value;
-    let receivingDate = document.getElementById('receivingDate').value;
-    let contactNumber = document.getElementById('contactNumber').value;
-    let eMail = document.getElementById('eMail').value;
-    let bank = document.getElementById('userBank').value;
-    let actual = document.getElementById('actualResidence').value;
-    if (sur == "") {
-        alert("Вы не указали Фамилию");
-    } else if (name == "") {
-        alert("Вы не указали Имя");
-    } else if (patronymic == "") {
-        alert("Вы не указали Отчество");
-    } else if (birth == "") {
-        alert("Вы не указали день рождения");
-    } else if (residence == "") {
-        alert("Вы не указали прописку");
-    } else if (serialNumber == "") {
-        alert("Вы не указали серийный номер");
-    } else if (personalNumber == "") {
-        alert("Вы не указали ИНН");
-    } else if (receivingDate == "") {
-        alert("Вы не указали дату получения");
-    } else if (contactNumber == "") {
-        alert("Вы не указали контактный номер");
-    } else if (eMail == "") {
-        alert("Вы не указали электронный адресс");
-    } else if (bank == "") {
-        alert("Вы не выбрали банк");
-    } else if (actual == "") {
-        alert("Вы не указали фактический адресс");
-    } else {
+        localStorage.userSurname = userSurname.value;
+        localStorage.userName = userName.value;
+        localStorage.userPatronymic = userPatronymic.value;
+        localStorage.userBirthday = userBirthday.value;
+        localStorage.userResidence = userResidence.value;
+        localStorage.serialNumber = serialNumber.value;
+        localStorage.personalNumber = personalNumber.value;
+        localStorage.receivingDate = receivingDate.value;
+        localStorage.contactNumber = contactNumber.value;
+        localStorage.eMail = eMail.value;
+        localStorage.userBank = userBank.value;
+        localStorage.actualResidence = actualResidence.value;
         window.location.href = 'form2.html';
     }
-}
-
 })
--- Июль/Megastore/script2.js
+++ Июль/Megastore/script2.js
@@ -1,94 +1,48 @@
-const form2 = document.querySelector('#form2');
+const form = document.querySelector('#form');
 
-form2.addEventListener('submit', () => {
-    const companyNameElement = document.querySelector('#companyName')
-    const userPositionElement = document.querySelector('#userPosition')
-    const monthSalaryElement = document.querySelector('#monthSalary')
-    const quarterSalaryElement = document.querySelector('#quarterSalary')
-    const productNameElement = document.querySelector('#productName')
-    const goodsPriceElement = document.querySelector('#goodsPrice')
-    const goodsAmountElement = document.querySelector('#goodsAmount')
-    const loanAmountElement = document.querySelector('#loanAmount')
-    const loanTypeElement = document.querySelector('#loanType')
-    const loanTermElement = document.querySelector('#loanTerm')
-    const pick_upPointElement = document.querySelector('#pick_upPoint')
+form.addEventListener('submit', () => {
+    const elements1 = document.querySelectorAll('#form input, #form select, #form textarea');
 
-    const credit = {};
-
-    localStorage.companyName = companyNameElement.value;
-    localStorage.userPosition = userPositionElement.value;
-    localStorage.monthSalary = monthSalaryElement.value;
-    localStorage.quarterSalary = quarterSalaryElement.value;
-    localStorage.productName = productNameElement.value;
-    localStorage.goodsPrice = goodsPriceElement.value;
-    localStorage.goodsAmount = goodsAmountElement.value;
-    localStorage.loanAmount = loanAmountElement.value;
-    localStorage.loanType = loanTypeElement.value;
-    localStorage.loanTerm = loanTermElement.value;
-    localStorage.pick_upPoint = pick_upPointElement.value;
-
-    function check2() {
-        let company = document.getElementById('companyName').value;
-        let position = document.getElementById('userPosition').value;
-        let monthSalary = document.getElementById('monthSalary').value;
-        let quarterSalary = document.getElementById('quarterSalary').value;
-        let product = document.getElementById('productName').value;
-        let price = document.getElementById('goodsPrice').value;
-        let amount = document.getElementById('goodsAmount').value;
-        let loanAmount = document.getElementById('loanAmount').value;
-        let loanType = document.getElementById('loanType').value;
-        let term = document.getElementById('loanTerm').value;
-        let pick_up = document.getElementById('pick_upPoint').value;
-        let button = document.getElementById('next2');
-        button.disabled = true;
-        if (company.length != 0 && position.length != 0 && monthSalary.length != 0 && quarterSalary.length != 0 && product.length != 0
-            && price.length != 0 && amount.length != 0 && loanAmount.length != 0 && loanType.length != 0
-            && term.length != 0 && pick_up.length != 0) {
-            button.disabled = false;
+    for(let i = 0; i < elements1.length; i++){
+        // console.log(elements1[i].value);
+        if (elements1[i].hasAttribute('required')){
+            if(elements1[i].value === '') {
+                // elements1[i].style.boxShadow = `"0 0 10px red";`
+                console.log('Поле пустое');
+            }
         }
+
+        const companyNameElement = document.querySelector('#companyName')
+        const userPositionElement = document.querySelector('#userPosition')
+        const monthSalaryElement = document.querySelector('#monthSalary')
+        const quarterSalaryElement = document.querySelector('#quarterSalary')
+        const productNameElement = document.querySelector('#productName')
+        const goodsPriceElement = document.querySelector('#goodsPrice')
+        const goodsAmountElement = document.querySelector('#goodsAmount')
+        const loanAmountElement = document.querySelector('#loanAmount')
+        const loanTypeElement = document.querySelector('#loanType')
+        const loanTermElement = document.querySelector('#loanTerm')
+        const pick_upPointElement = document.querySelector('#pick_upPoint')
+
+        const credit = {};
+
+        localStorage.companyName = companyNameElement.value;
+        localStorage.userPosition = userPositionElement.value;
+        localStorage.monthSalary = monthSalaryElement.value;
+        localStorage.quarterSalary = quarterSalaryElement.value;
+        localStorage.productName = productNameElement.value;
+        localStorage.goodsPrice = goodsPriceElement.value;
+        localStorage.goodsAmount = goodsAmountElement.value;
+        localStorage.loanAmount = loanAmountElement.value;
+        localStorage.loanType = loanTypeElement.value;
+        localStorage.loanTerm = loanTermElement.value;
+        localStorage.pick_upPoint = pick_upPointElement.value;
+
+
+        //  let userInfo = (localStorage);
+        // const totalInfo = {...credit, ...userInfo};
+        // console.log(totalInfo);
+
     }
-
-    check2();
-
-    function send2() {
-        let company = document.getElementById('companyName').value;
-        let position = document.getElementById('userPosition').value;
-        let monthSalary = document.getElementById('monthSalary').value;
-        let quarterSalary = document.getElementById('quarterSalary').value;
-        let product = document.getElementById('productName').value;
-        let price = document.getElementById('goodsPrice').value;
-        let amount = document.getElementById('goodsAmount').value;
-        let loanAmount = document.getElementById('loanAmount').value;
-        let loanType = document.getElementById('loanType').value;
-        let term = document.getElementById('loanTerm').value;
-        let pick_up = document.getElementById('pick_upPoint').value;
-        if (company == "") {
-            alert("Вы не указали наименование организации");
-        } else if (position == "") {
-            alert("Вы не указали должность");
-        } else if (monthSalary == "") {
-            alert("Вы не указали зароботную плату в месяц");
-        } else if (quarterSalary == "") {
-            alert("Вы не указали заработную плату за квартал");
-        } else if (product == "") {
-            alert("Вы не указали наименование товара");
-        } else if (price == "") {
-            alert("Вы не указали стоимость товара");
-        } else if (amount == "") {
-            alert("Вы не указали колличество товара");
-        } else if (loanAmount == "") {
-            alert("Вы не указали сумму кредита");
-        } else if (loanType == "") {
-            alert("Вы не указали вид кредита");
-        } else if (term == "") {
-            alert("Вы не указали срок кредита");
-        } else if (pick_up == "") {
-            alert("Вы не выбрали точку получения товара");
-        } else {
-            let userInfo = (localStorage);
-            const totalInfo = {...credit, ...userInfo};
-            console.log(totalInfo);
-        }
-    }
-
 })
+console.log(localStorage);
