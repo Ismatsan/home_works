66fc1456320e018c43464ff93b8921b20560090d
Ismatsan <ismat7300@gmail.com>
2022-07-30 15:23:56 +0600

    feat: added_findErrorElem_function

--- Megastore/script.js
+++ Megastore/script.js
@@ -12,32 +12,45 @@ const emailField = document.querySelector('#eMail');
 const bankField = document.querySelector('#userBank');
 const actualField = document.querySelector('#actualResidence');
 
+//находит потомков инпут с тегом "р" и если у него есть атрибут 'text-error' то удаляет его
+const findErrorElem =() => {
+    const errorElem = document.querySelectorAll('input > p:last-child');
+    for (let i = 0; i < errorElem.length; i++) {
+        if (errorElem[i].hasAttribute('text-error')) {
+            // errorElem[i].classList.remove('text-error');
+            errorElem[i].document.querySelector('.p_teg > p').remove();
+        }
+    }
+}
+
+const createErrorElem =() =>{
+    const elem = document.createElement('p');
+    elem.className ='text-error';
+    elem.innerText = 'Обязательное поле';
+    return elem;
+}
+
+const checkElem =(event) =>{
+    if (event.target.value !==''){
+        event.target.classList.remove('empty-field');
+        event.target.nextElementSibling.remove();
+    }
+}
+
 form.addEventListener('submit', () =>{
     const elements = document.querySelectorAll('#form input, #form select, #form textarea');
     for(let i = 0; i < elements.length; i++){
         if(elements[i].hasAttribute('data-required')){
             if (elements[i].value === '') {
                 elements[i].classList.add('empty-field');
-                let inform = document.createElement('p');
-                inform.innerText = 'Заполните поле';
-                elements[i].after(inform);
-
-                return
+              findErrorElem()
+                elements[i].after(createErrorElem())
+                elements[i].addEventListener('input', checkElem)
+                // return
             }
         }
-        elements[i].classList.remove('empty-field');
-        //удаляем тег р с помощю css
-        // document.p.classList.toggle('par');
-
     }
-//       находим параграф, который будем удалять
-//      let p_tegInput = document.querySelector("input.p_teg");
-//    let removableNode = document.querySelectorAll("input.p_teg p")[0];
-//    p_tegInput.removeChild(removableNode);
 })
-
-//
-
 
 surnameField.addEventListener('change', () =>{
     console.log(surnameField.value);
